2f1471cafc23b2d3c07aca725598203bcbd4ce6a
Luís Clício <cliciosa14@gmail.com>
2020-09-20 12:06:03 -0300

    feat: Create 'add task' interface and function to add task

---
+++ resources/js/app/handleAddTask.js
@@ -0,0 +1,40 @@
+import generateFormData from '../utils/generateFormData.js'
+import handleDataInLocalStorage from '../utils/handleDataInLocalStorage.js'
+import handleApiData from '../utils/handleApiData.js'
+import handleLocationURL from '../utils/handleLocationURL.js'
+import handleErrorMessageView from '../utils/handleErrorMessageView.js'
+import { toggleAddTaskArea } from './toggleAddTaskArea.js'
+
+export default () => {
+  const formElement = document.querySelector('.js-form-add-task');
+
+  formElement.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = generateFormData(formElement);
+    const { token } = handleDataInLocalStorage.getData();
+
+    const data = await handleApiData(
+      '/user/todos',
+      'POST',
+      formData,
+      { Authorization: token }
+    );
+
+    if (data.error === true && data.auth === false) {
+      handleErrorMessageView(data);
+      handleLocationURL('/form/login', 2000);
+      return;
+    }
+
+    if (data.error === false) {
+      console.log(data);
+      // Add task in DOM
+      formElement.reset();
+      toggleAddTaskArea();
+      return;
+    }
+
+    return handleErrorMessageView(data);
+  });
+}
--- resources/js/app/toggleAddTaskArea.js
+++ resources/js/app/toggleAddTaskArea.js
@@ -1,9 +1,11 @@
-export default () => {
-  const btnAddTaskTrigger = document.querySelector('.js-add-task-trigger');
-  const addTaskArea = document.querySelector('.js-add-task-modal');
+const btnAddTaskTrigger = document.querySelector('.js-add-task-trigger');
+const addTaskArea = document.querySelector('.js-add-task-modal');
 
-  btnAddTaskTrigger.addEventListener('click', () => {
-    btnAddTaskTrigger.classList.toggle('is-active');
-    addTaskArea.classList.toggle('modal-task--hidden');
-  });
+export const toggleAddTaskArea = () => {
+  btnAddTaskTrigger.classList.toggle('is-active');
+  addTaskArea.classList.toggle('modal-task--hidden');
+}
+
+export const toggleAddTaskAreaWithClick = () => {
+  btnAddTaskTrigger.addEventListener('click', toggleAddTaskArea);
 }
